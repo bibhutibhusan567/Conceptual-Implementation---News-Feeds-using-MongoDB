ab06a423174797749cd4b4e67351249f788bc0db
bibhutibhusan567 <bibhutibhusan567@gmail.com>
2020-12-16 22:54:24 +0530

    lets try

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 8080
+const { newsArticleModel } = require('./connector');
 
 const onePageArticleCount = 10
 
@@ -8,6 +9,11 @@ const onePageArticleCount = 10
 // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.get('/newFeeds', async (req, res) => {
+    res.send(await newsArticleModel.find().skip(req.query.offset || 0).limit(req.query.limit || 10));
+
+});
 
 
 
