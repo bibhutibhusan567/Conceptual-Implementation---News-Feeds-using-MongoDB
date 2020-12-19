167676fb974e7fc3a0b1bc3f33663ecdc733c2c9
bibhutibhusan567 <bibhutibhusan567@gmail.com>
2020-12-19 10:42:48 +0530

    done

--- src/index.js
+++ src/index.js
@@ -10,38 +10,13 @@ const onePageArticleCount = 10
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-//const isNullOrUndefinedOrNan = (val)=> val !==null || val !==undefined || isNan(val);
+//const isNullOrUndefinedOrNan = (val) => val === null || val === undefined || isNan(val);
 app.get('/newFeeds', async (req, res) => {
-    // const offset = isNullOrUndefinedOrNan(req.query.offset) ? 0 : Number(req.query.offset);
-    // const limit = isNullOrUndefinedOrNan(req.query.limit) ? 10 : Number(req.query.limit);
+    // const offset = isNullOrUndefinedOrNan(req.query.offset) ? Number(req.query.offset) : 0;
+    // const limit = isNullOrUndefinedOrNan(req.query.limit) ? Number(req.query.limit) : 10;
     res.send(await newsArticleModel.find().skip(Number(req.query.offset) || 0).limit(Number(req.query.limit) || 10));
 
 });
-
-
-
-app.listen(port, () => console.log(`App listening on port ${port}!`))
-
-module.exports = app;
-// const express = require('express')
-// const app = express()
-// const port = 8080
-// const { newsArticleModel } = require('./connector');
-
-// const onePageArticleCount = 10
-
-
-// // Parse JSON bodies (as sent by API clients)
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.json());
-
-// //const isNullOrUndefinedOrNan = (val)=> val !==null || val !==undefined || isNan(val);
-// app.get('/newFeeds', async (req, res) => {
-//     // const offset = isNullOrUndefinedOrNan(req.query.offset) ? 0 : Number(req.query.offset);
-//     // const limit = isNullOrUndefinedOrNan(req.query.limit) ? 10 : Number(req.query.limit);
-//     res.send(await newsArticleModel.find().skip(Number(req.query.offset) || 0).limit(Number(req.query.limit) || 10));
-
-// });
 
 
 
